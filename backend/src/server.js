7b88a4ec438253c600c30c393c5de65ee20d024b
M-Arsalan-S <marsalans704@gmail.com>
2025-07-05 11:41:33 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"; // its a browser security rule
 import dotenv from "dotenv"; // for using env variable
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,14 +11,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // connectDB();
 
 // middleware - famous use case is auth check, here we do rate limiting
 // app.use(cors()); OR  (must be at the beginning)
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -28,6 +34,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 // What is an Endpoint/Route?
 // It's a combination of URL+HTTP method that lets the client interact with a specific resource.
